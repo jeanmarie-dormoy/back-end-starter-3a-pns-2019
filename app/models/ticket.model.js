2b0329fd8126c9d172fe97e2b77497dda07eba91
Jean-Yves Delmotte <delmotte.jy@gmail.com>
2019-02-21 20:19:49 +0100

    rename student to author

--- app/models/ticket.model.js
+++ app/models/ticket.model.js
@@ -4,5 +4,5 @@ const BaseModal = require('../utils/base-model.js');
 module.exports = new BaseModal('Ticket', {
   title: Joi.string().required(),
   date: Joi.date().required(),
-  student: Joi.string().required(),
+  author: Joi.string().required(),
 });
