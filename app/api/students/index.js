f9d474ae5fc372390737a8238f6236c5484c1046
BauceCamille <camille.bauce@etu.unice.fr>
2019-03-12 02:39:25 +0100

    cleaning code and rename

--- app/api/students/index.js
+++ app/api/students/index.js
@@ -2,17 +2,18 @@ const { Router } = require('express');
 const { Student } = require('../../models');
 
 const router = new Router();
+
 const getStudentByName = function (name) {
   return Student.get().filter(
     student => student.firstName.includes(name) || student.lastName.includes(name),
   );
 };
+
 router.get('/', (req, res) => res.status(200).json(Student.get()));
-router.get('/:studentId', (req, res) => {
-  res.status(200).json(Student.getById(req.params.studentId));
-});
+router.get('/:studentId', (req, res) => res.status(200).json(Student.getById(req.params.studentId)));
 router.delete('/:studentId', (req, res) => res.status(200).json(Student.delete(req.params.studentId)));
 router.put('/:studentId', (req, res) => res.status(200).json(Student.update(req.params.studentId, req.body)));
+
 router.post('/', (req, res) => {
   try {
     const ticket = Student.create(req.body);
--- app/api/tickets/index.js
+++ app/api/tickets/index.js
@@ -1,13 +1,10 @@
 const { Router } = require('express');
 const { Ticket, Student } = require('../../models');
-const logger = require('../../utils/logger');
+// const logger = require('../../utils/logger');
 
 const router = new Router();
 
 const attachStudent = function (ticket) {
-  /* const studId = ticket.studentID;
-  const stud = Student.getById(studId);
-  Ticket.update('student', stud); */
   const ticketCopy = ticket.valueOf();
   ticketCopy.student = Student.getById(ticket.studentID);
   return ticketCopy;
@@ -15,10 +12,9 @@ const attachStudent = function (ticket) {
 
 const getTickets = function () {
   let i;
-  // logger.info('Salut Ghetto JMD');
   const res = [];
-  for (i = 0; i < Ticket.items.length; i += 1) {
-    res.push(attachStudent(Ticket.items[i]));
+  for (i = 0; i < Ticket.get().length; i += 1) {
+    res.push(attachStudent(Ticket.get()[i]));
   }
   return res;
 };
@@ -26,37 +22,24 @@ const getTickets = function () {
 const getTicketById = function (id) {
   let idstr;
   if (typeof id === 'string') idstr = parseInt(id, 10);
-  const ticket = getTickets()
-    .find(i => i.id === idstr);
-  return ticket;
+  return getTickets().find(i => i.id === idstr);
 };
 
 const getTicketsByStudentId = function (studId) {
-  // let studIdInt;
-  // if (typeof studId === 'string') studIdInt = parseInt(studId, 10);
-  const temp = getTickets().filter(ticket => ticket.studentID === +studId);
-  logger.log('temp ==> ', temp);
-  return temp;
+  return getTickets().filter(ticket => ticket.studentID === +studId);
 };
 
-// router.get('/', (req, res) => res.status(200).json(Ticket.get()));
 router.get('/', (req, res) => res.status(200).json(getTickets()));
-router.get('/statusJmd', (req, res) => res.status(200).json('ok JMD.'));
-router.get('/attachJmd', (req, res) => res.status(200).json(getTickets()));
+router.get('/ticketsByStudId/:studentId', (req, res) => res.status(200).json(getTicketsByStudentId(req.params.studentId)));
+router.get('/:ticketId', (req, res) => res.status(200).json(attachStudent(getTicketById(req.params.ticketId))));
 
-router.get('/ticketsByStudId/:studId', (req, res) => res.status(200).json(getTicketsByStudentId(req.params.studId)));
+router.delete('/:ticketId', (req, res) => res.status(200).json(Ticket.delete(req.params.ticketId)));
 
-router.get('/:ticketId', (req, res) => {
-  res.status(200).json(attachStudent(getTicketById(req.params.ticketId)));
-});
-router.delete('/:ticketId', (req, res) => {
-  res.status(200).json(Ticket.delete(req.params.ticketId));
-});
 router.put('/:ticketId', (req, res) => res.status(200).json(attachStudent(Ticket.update(req.params.ticketId, req.body))));
+
 router.post('/', (req, res) => {
   try {
     const ticket = Ticket.create(req.body);
-    // attachStudent(ticket);
     res.status(201).json(attachStudent(ticket));
   } catch (err) {
     if (err.name === 'ValidationError') {
